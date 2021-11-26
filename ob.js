ea405d1b1a911bd1c9248e00dbe01a03c1b7e206
khushi1711 <90022273+khushi1711@users.noreply.github.com>
2021-11-26 16:52:05 +0530

    Add files via upload

---
+++ ob.js
@@ -0,0 +1,25 @@
+var Person={
+    p1:{
+firstName:"khushi",
+lastName:"kumari",
+OfficeAddress:"H67,gaya"},
+
+p2:{
+    firstName:"Anjali",
+lastName:"kumari",
+OfficeAddress:"B7,Patna"},
+
+
+ p3:{
+    firstName="Riya",
+lastName="Singh",
+OfficeAddress="Bhuj,Gujarat",},};
+console.log("name"+ Person.p1.firstName);
+console.log(Person.p1.lastName);
+console.log(Person.p1.OfficeAddress); 
+console.log(Person.p2.firstName);
+console.log(Person.p2.lastName);
+console.log(Person.p2.OfficeAddress); 
+console.log(Person.p3.firstName);
+console.log(Person.p3.lastName);
+console.log(Person.p3.OfficeAddress); 
